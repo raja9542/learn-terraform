d445ba0a1f06e5e486bb6cae4c7a4266db3ff34d
raja9542 <rajasekhar1banda@gmail.com>
2023-11-06 13:03:36 +0530

    terraform datasources

--- data-sources/data.tf
+++ data-sources/data.tf
@@ -13,9 +13,14 @@ data "aws_instance" "foo" {
   instance_id = "i-0b57d513ef3cad305"
 }
 
-output "instance" {
+output "public_ip" {
   value = data.aws_instance.foo.public_ip
 }
+
+output "private_ip" {
+  value = data.aws_instance.foo.private_ip
+}
+
 #Arguments are inputs which we will provide and as an output we will fetch attributes
 #most_recent
 #name_regex
