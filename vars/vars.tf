fb66509d20b832de4dee6eaf09c9f506bca85a53
raja9542 <rajasekhar1banda@gmail.com>
2023-11-06 15:22:33 +0530

    terraform variable types list map default

--- vars/vars.tf
+++ vars/vars.tf
@@ -5,3 +5,72 @@ variable "sample" {
 output "sample" {
   value = var.sample
 }
+
+# string data type
+
+variable "sample1" {
+  default = "Hello World"
+}
+
+output "sample1" {
+  value = var.sample1
+}
+
+# Number data type
+
+variable "sample2" {
+  default = 100
+}
+
+output "sample2" {
+  value = var.sample2
+}
+
+# Boolean data type
+
+variable "sample3" {
+  default = true
+}
+
+output "sample3" {
+  value = var.sample3
+}
+
+# Default variable type
+
+variable "sample4" {
+  default = "Hello Raja"
+}
+
+output "sample4" {
+  value = var.sample4
+}
+
+# List variable type
+
+variable "sample5" {
+  default = [
+  100,
+  "xyz",
+  "abc"
+  ]
+}
+# indexing starts from 0 1 2 3... we need 2nd value
+output "sample5" {
+  value = var.sample5[1]
+}
+
+# Map variable type
+
+variable "sample6" {
+  default= {
+    number= 10
+    string= "Hello"
+    boolean = "true"
+  }
+}
+
+output "sample6" {
+  value = var.sample6["number"]
+}
+
