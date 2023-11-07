f44765fea7881ef7b9f22a56e3945710098efc3a
raja9542 <rajasekhar1banda@gmail.com>
2023-11-07 16:15:10 +0530

    terraform functions

---
+++ for-each-module/main.tf
@@ -0,0 +1,22 @@
+variable "components" {
+  default = {
+    cart = {
+      name = "cart",
+      instance_type= "t3.small"
+    }
+    catalogue = {
+      name = "catalogue",
+      instance_type= "t3.micro"
+    }
+
+  }
+}
+ module "ec2" {
+   source = "./module"
+   for_each = var.components
+   name= each.value.name
+   instance_type = each.value.instance_type
+ }
+
+## always iterate modules, not the resources
+## always map the data and use for_each loop
---
+++ for-each-module/module/main.tf
@@ -0,0 +1,26 @@
+# if data is Map
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.centos8.id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = var.name
+  }
+}
+
+data "aws_ami" "centos8" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+
+}
+
+#output "public_ip" {
+#  value = {
+#    for k, v in aws_instance.web : k => v.public_ip
+#  }
+#}
+
+
+variable "name" {}
+variable "instance_type" {}
